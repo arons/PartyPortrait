c9c78032ecae97ff73ab672b8d2814108c593337
Renzo Dani <arons7@gmail.com>
2017-09-19 21:08:58 +0200

    test with text

--- src/PartyPictures.java
+++ src/PartyPictures.java
@@ -1,5 +1,8 @@
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -9,6 +12,9 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -28,7 +34,7 @@ public class PartyPictures extends JFrame {
 
 	// hard coded config
 	private static boolean FULLSCREEN = true;
-	
+
 	private static final String fileNamePatter = "yyyy_MM_dd_HHmmss";
 	private static final String fileExt = ".jpg";
 
@@ -42,8 +48,15 @@ public class PartyPictures extends JFrame {
 	Timer saverTimer = new Timer(2000, new UpdateSaver());
 
 	boolean running = false;
-	
-	private static Object syncObj= new Object();
+
+	private enum Status {
+		RANDOM_COLLAGE, FULL_PHOTO, MESSAGE_DISPLAY
+	}
+
+	private Status status = Status.MESSAGE_DISPLAY;
+	private String message = "Starting...";
+
+	private static Object syncObj = new Object();
 
 	public static void main(String[] args) throws Exception {
 		new PartyPictures();
@@ -77,21 +90,52 @@ public class PartyPictures extends JFrame {
 	}
 
 	public void paint(Graphics g) {
-		if (full != null) {
-			g.drawImage(full, 0, 0, this);
+		switch (status) {
+		case MESSAGE_DISPLAY:
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, getWidth(), getHeight());
+			g.setColor(Color.BLACK);
+			Font font = new Font("San Serif", Font.PLAIN, 60);
+			g.setFont(font);
+			
+			Graphics2D g2d = (Graphics2D) g;
+			FontRenderContext frc = g2d.getFontRenderContext();
+		    TextLayout layout = new TextLayout(message, font, frc);
+		    Rectangle2D bounds = layout.getBounds();
+
+		    int width = (int) Math.round(bounds.getWidth());
+		    int height = (int) Math.round(bounds.getHeight());
+		    int sx = (getWidth() - width) / 2;
+		    int sy = height + (getHeight() - height) / 2;
+
+		    layout.draw(g2d, (float) sx, (float) sy);
+		    
+			break;
+		case FULL_PHOTO:
+			if (full != null) {
+				g.drawImage(full, 0, 0, this);
+			}
+			break;
+		case RANDOM_COLLAGE:
+			if (saver != null) {
+				int x = 0, y = 0;
+				for (int i = 0; i < saver.length; i++) {
+					for (int j = 0; j < saver[i].length; j++) {
+						g.drawImage(saver[i][j], x, y, this);
+						x = x + getWidth() / saver[i].length;
+					}
+					x = 0;
+					y = y + getHeight() / saver.length;
+				}
+			}
+			break;
+		default:
+			break;
 		}
 
-		if (saver != null) {
-			int x = 0, y = 0;
-			for (int i = 0; i < saver.length; i++) {
-				for (int j = 0; j < saver[i].length; j++) {
-					g.drawImage(saver[i][j], x, y, this);
-					x = x + getWidth() / saver[i].length;
-				}
-				x = 0;
-				y = y + getHeight() / saver.length;
-			}
-		}
+		
+
+		
 	}
 
 	private class UpdateSaver implements ActionListener {
@@ -117,6 +161,7 @@ public class PartyPictures extends JFrame {
 				img = img.getScaledInstance(getWidth() / saver.length, getHeight() / saver[x].length, Image.SCALE_FAST);
 				saver[x][y] = img;
 				full = null;
+				status = Status.RANDOM_COLLAGE;
 				repaint();
 			}
 		}
@@ -128,40 +173,50 @@ public class PartyPictures extends JFrame {
 			System.exit(0);
 		}
 	}
-	
-	
+
 	/**
 	 * 
 	 *
 	 */
-	private class PhotoThread implements Runnable{
-		
+	private class PhotoThread implements Runnable {
+
 		public void run() {
-			
+
 			synchronized (syncObj) {
 				running = true;
-			} 
+			}
+			message = "Sorridi";
+			status = Status.MESSAGE_DISPLAY;
+			repaint();
 			
+
 			SimpleDateFormat df = new SimpleDateFormat(fileNamePatter);
 			String filename = df.format(new Date()) + fileExt;
 			System.out.println("Filename:" + filename);
 			try {
 				Process p = Runtime.getRuntime().exec("gphoto2 --capture-image-and-download --filename=" + filename);
+				message = "...aspetta...";
+				repaint();
 				p.waitFor();
-				
+
 				full = Toolkit.getDefaultToolkit().getImage(filename);
 				full = full.getScaledInstance(getWidth(), -1, Image.SCALE_FAST);
-				
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			} catch (java.lang.InterruptedException ex) {
-				System.out.println(ex.getMessage());
+
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+				message = "Error:"+e.getMessage();
+				repaint();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+				}
 			} finally {
-				
+
 				synchronized (syncObj) {
 					running = false;
-					
+
 					init(saver);
+					status = Status.FULL_PHOTO;
 					repaint();
 
 					// randomize and initialize random files
@@ -178,9 +233,8 @@ public class PartyPictures extends JFrame {
 					Collections.shuffle(Arrays.asList(randomOrder), r);
 
 					saverTimer.start();
-				} 
-				
-				
+				}
+
 			}
 		}
 
@@ -200,20 +254,19 @@ public class PartyPictures extends JFrame {
 		@Override
 		public void keyPressed(KeyEvent e) {
 			System.out.println("Key event received: " + e);
-			if(e.getKeyChar() == 'e'){
+			if (e.getKeyChar() == 'e') {
 				System.exit(0);
 			}
-			
+
 			synchronized (syncObj) {
 				if (running) {
 					return;
 				}
 				saverTimer.stop();
 				PhotoThread pt = new PhotoThread();
-				new Thread( pt ).start();
-			} 
+				new Thread(pt).start();
+			}
 
-			
 		}
 	}
 }
